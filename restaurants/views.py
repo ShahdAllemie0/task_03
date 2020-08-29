6c54e56b8d3836caf8de9aff133969f7b30fd9e3
Shahd Allemie <shahdallemie@Shahds-MacBook-Pro.local>
2020-08-29 16:38:52 +0300

    Task3 is done

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,14 +6,18 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
-    }
+    "my_list":[
+       {"restaurant_name" :"Piatto" ,"food_type":"Italian"},
+       {"restaurant_name" :"Benihana" , "food_type":"Japanese" },
+       {"restaurant_name" : "Nozomi", "food_type":"Japanese"}
+       ]}
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
     context = {
+    "my_object":{"restaurant_name" :"Piatto" ,"food_type":"Italian"}
 
     }
     return render(request, 'detail.html', context)
